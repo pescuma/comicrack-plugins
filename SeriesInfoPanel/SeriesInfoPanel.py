d33615ab117c7aca39136d92ec967e740a7bbeca
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2010-08-30 04:57:48 +0000

    SeriesInfoPanel:  - Moved all files to root folder  - Added issue page

--- SeriesInfoPanel/SeriesInfoPanel.py
+++ SeriesInfoPanel/SeriesInfoPanel.py
@@ -20,7 +20,6 @@ Boston, MA 02111-1307, USA.
 _SCRIPT_DIRECTORY =  __file__[:-len('SeriesInfoPanel.py')] 
 
 import sys
-sys.path.append(_SCRIPT_DIRECTORY + 'stdlib')
 import clr
 clr.AddReference('System.Drawing')
 
@@ -32,13 +31,98 @@ import time
 # Globals
 oldTmpFiles = []
 seriesTemplate = tempita.HTMLTemplate.from_filename(_SCRIPT_DIRECTORY + 'series.tmpl')
+issueTemplate = tempita.HTMLTemplate.from_filename(_SCRIPT_DIRECTORY + 'issue.tmpl')
 
+
+def ToString(v):
+	if v is None:
+		return ''
+	if not isinstance(v, basestring):
+		return str(v)
+	return v
+
+def ToInt(v):
+	v = ToString(v)
+	if v.isdigit():
+		return int(v)
+	else:
+		return 0
 
 def DeleteOldTmpFiles():
 	global oldTmpFiles
 	for file in oldTmpFiles:
 		System.IO.File.Delete(file)
 	oldTmpFiles = []
+	
+	
+class BookWrapper:
+	_emptyVals = { 
+		'Count' : '-1', 
+		'Year' : '-1', 
+		'Month' : '-1', 
+		'Volume' : '-1', 
+		'AlternateCount' : '-1', 
+		'Rating' : '-1'
+		}
+	
+	def __init__(self, book):
+		self.raw = book
+		self._cover = None
+	
+	def _get(self, name):
+		try:
+			return ToString(getattr(self.raw, name)).strip()
+		except:
+			return ''
+	
+	def __getattr__(self, name):
+		if name == 'Cover':
+			return self.GetCover()
+		
+		if name in self._emptyVals:
+			emptVal = self._emptyVals[name]
+		else:
+			emptVal = ''
+			
+		ret = self._get(name)
+		if ret == '' or ret == emptVal:
+			ret = self._get("Shadow" + name)
+		if ret == '' or ret == emptVal:
+			ret = ''
+		return ret
+	
+	def GetCover(self, height = 120):
+		global oldTmpFiles
+		
+		if not (self._cover is None):
+			return self._cover
+		
+		coverIndex = 0 
+		if self.raw.FrontCoverPageIndex > 0:
+			coverIndex = self.raw.FrontCoverPageIndex
+		image = ComicRack.App.GetComicPage(self.raw, coverIndex)
+		
+		if image is None:
+			self._cover = ''
+			return ''
+
+		tmpFile = System.IO.Path.GetTempFileName()
+		oldTmpFiles.append(tmpFile)
+
+		# We need a jpg
+		self._cover = tmpFile + '.jpg'
+		oldTmpFiles.append(self._cover)
+		#print self._cover
+		
+		try:
+			image = ResizeImage(image, height)
+			image.Save(self._cover, System.Drawing.Imaging.ImageFormat.Jpeg)
+			return self._cover
+		except Exception,e:
+			print '[SeriesInfoPanel] Exception when saving image: ', e
+			self._cover = ''
+			return ''
+
 
 class DB:
 	def __init__(self):
@@ -79,7 +163,7 @@ class Volume:
 		ret = []
 		
 		for book in self.issues:
-			n = GetNumber(book)
+			n = book.Number
 			if n in nums:
 				ret.append(n)
 			else:
@@ -91,7 +175,7 @@ class Volume:
 		nums = set()
 		
 		for book in self.issues:
-			nums.add(GetNumberInt(book))
+			nums.add(ToInt(book.Number))
 		
 		nums.discard(0)
 		
@@ -112,7 +196,7 @@ class Volume:
 	def GetReadPercentage(self):
 		read = 0
 		for book in self.issues:
-			read += book.ReadPercentage
+			read += ToInt(book.ReadPercentage)
 		read = float(read) / len(self.issues)
 		
 		# Avoid 100% when missing only one page
@@ -121,34 +205,6 @@ class Volume:
 		else:
 			return int(read)
 
-def GetSeriesName(book):
-	ret = book.Series.strip()
-	if ret == '':
-		ret = book.ShadowSeries.strip()
-	if ret == '':
-		ret = ''
-	return ret
-
-def GetVolumeName(book):
-	ret = str(book.Volume)
-	if ret == '' or ret == '-1':
-		ret = str(book.ShadowVolume)
-	if ret == '' or ret == '-1':
-		ret = ''
-	return ret
-
-def GetNumber(book):
-	ret = book.Number.strip()
-	if ret == '':
-		ret = book.ShadowNumber.strip()
-	return ret
-
-def GetNumberInt(book):
-	ret = GetNumber(book)
-	if ret.isdigit():
-		return int(ret)
-	else:
-		return 0
 
 def ResizeImage(image, height):
 	result = System.Drawing.Bitmap(image.Width * height / image.Height, height)
@@ -161,53 +217,30 @@ def ResizeImage(image, height):
 	
 	return result
 
-def GetCoverImagePath(book, height):
-	global oldTmpFiles
-	
-	coverIndex = 0 
-	if book.FrontCoverPageIndex > 0:
-		coverIndex = book.FrontCoverPageIndex
-	image = ComicRack.App.GetComicPage(book, coverIndex)
-	
-	if image is None:
-		return ''
-
-	coverFile = System.IO.Path.GetTempFileName()
-	oldTmpFiles.append(coverFile)
-
-	# We need a jpg
-	coverFile += '.jpg'
-	oldTmpFiles.append(coverFile)
-	
-	#print coverFile
-	
-	try:
-		image = ResizeImage(image, height)
-		image.Save(coverFile, System.Drawing.Imaging.ImageFormat.Jpeg)
-		return coverFile
-	except Exception,e:
-		print '[SeriesInfoPanel] Exception when saving image: ', e
-		return ''
 
 class Placeholder:
 	pass
 
-#@Name Series Info Panel
-#@Hook ComicInfoHtml
-#@Enabled true
-#@Description Show information about selected series
-def SeriesHtmlInfoPanel(books):
-	# Cleanup old temps. I need to do this better.
-	DeleteOldTmpFiles()
+
+def GenerateHTMLForIssue(book):
+	book = BookWrapper(book)
 	
+	issueTemplate = tempita.HTMLTemplate.from_filename(_SCRIPT_DIRECTORY + 'issue.tmpl')
+	html = issueTemplate.substitute(book=book, info='')
+	
+	#print html
+	return html
+
+def GenerateHTMLForSeries(books):
 	db = DB()
 	
 	info = []
 	
 	finishTime = time.clock() + 1
 	for book in books:
-		series = db.GetSeries(GetSeriesName(book))
-		volume = series.GetVolume(GetVolumeName(book))
+		book = BookWrapper(book)
+		series = db.GetSeries(book.Series)
+		volume = series.GetVolume(book.Volume)
 		volume.issues.append(book)
 		
 		# Avoid look forever
@@ -235,10 +268,10 @@ def SeriesHtmlInfoPanel(books):
 			s.volumes.append(v)
 			
 			v.name = volume.name
-			v.cover = GetCoverImagePath(volume.issues[0], 120)
+			v.cover = volume.issues[0].Cover
 			
-			firstNum = GetNumber(volume.issues[0])
-			lastNum = GetNumber(volume.issues[-1])
+			firstNum = volume.issues[0].Number
+			lastNum = volume.issues[-1].Number
 			if firstNum != lastNum:
 				v.issues = firstNum + ' to ' + lastNum
 			else:
@@ -258,3 +291,20 @@ def SeriesHtmlInfoPanel(books):
 	
 	#print html
 	return html
+
+
+#@Name Series Info Panel
+#@Hook ComicInfoHtml
+#@Enabled true
+#@Description Show information about selected series
+def SeriesHtmlInfoPanel(books):
+	# Cleanup old temps. I need to do this better.
+	DeleteOldTmpFiles()
+	
+	numBooks = len(books)
+	if numBooks < 1:
+		return ''
+	elif numBooks == 1:
+		return GenerateHTMLForIssue(books[0])
+	else:
+		return GenerateHTMLForSeries(books)

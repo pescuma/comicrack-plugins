0c518500c6f7ee4ca89eba1a760e31dad1fafd69
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2010-09-08 03:34:08 +0000

    NextIssuesToRead: 0.1

---
+++ NextIssuesToRead/NextIssuesToRead.py
@@ -0,0 +1,126 @@
+﻿"""
+Copyright (C) 2010 Ricardo Pescuma Domenecci
+
+This is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this file; see the file license.txt.  If
+not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+"""
+
+import time
+
+from _utils1 import *
+from _db1 import *
+
+
+def GetSeries(book):
+	if book.Series:
+		return book.Series
+	else:
+		return book.ShadowSeries
+def GetVolume(book):
+	if book.Volume:
+		return book.Volume
+	else:
+		return book.ShadowVolume
+def GetNumber(book):
+	if book.Number:
+		return book.Number
+	else:
+		return book.ShadowNumber
+
+
+def StartedReading(book):
+	return book.OpenedCount > 0 and (book.ReadPercentage > 50 or book.LastPageRead >= book.FrontCoverPageIndex + 3)
+
+def HasStartedReadIssue(volume):
+	for issue in volume.issues:
+		if StartedReading(issue):
+			return True
+	return False
+
+
+#@Name	 Next issues to read
+#@Hook	 CreateBookList
+#@PCount 0
+#@Enabled true
+#@Description List next books to read in series that you already started reading
+def NextIssuesToRead(books, a, b):
+	db = DB()
+	
+	#print '[NextIssuesToRead] Loading books from db'
+	#dt = time.clock()
+	
+	# Load books
+	for book in books:
+		series = db.GetSeries(GetSeries(book))
+		volume = series.GetVolume(GetVolume(book))
+		volume.issues.append(book)
+	
+	#dt = time.clock() - dt
+	#print '[NextIssuesToRead] Loaded in ', dt, 's'
+	
+	#print '[NextIssuesToRead] Removing not read volumes'
+	#dt = time.clock()
+	
+	# Remove volumes that have not book marked as started read
+	for series in db.series.itervalues():
+		toremove = []
+		
+		for volumeKey in series.volumes.iterkeys():
+			volume = series.volumes[volumeKey]
+			
+			if not HasStartedReadIssue(volume):
+				toremove.append(volumeKey)
+		
+		for volumeKey in toremove:
+			del series.volumes[volumeKey]
+	
+	#dt = time.clock() - dt
+	#print '[NextIssuesToRead] Removed in ', dt, 's'
+	
+	#print '[NextIssuesToRead] Sorting issues'
+	#dt = time.clock()
+	
+	# Sort
+	for series in db.series.itervalues():
+		for volume in series.volumes.itervalues():
+			volume.sort()
+	
+	#dt = time.clock() - dt
+	#print '[NextIssuesToRead] Sorted in ', dt, 's'
+	
+	#print '[NextIssuesToRead] Finding next ones to read'
+	#dt = time.clock()
+
+	# Generate list
+	ret = []
+	for series in db.series.itervalues():
+		for volume in series.volumes.itervalues():
+			addNext = False
+			for book in volume.issues:
+				if StartedReading(book):
+					if book.ReadPercentage == 100:
+						addNext = True
+					else:
+						ret.append(book)
+						addNext = False
+				else:
+					if addNext:
+						ret.append(book)
+						addNext = False
+	
+	#dt = time.clock() - dt
+	#print '[NextIssuesToRead] Generated list in ', dt, 's'
+	
+	return ret
---
+++ NextIssuesToRead/_utils1.py
@@ -0,0 +1,217 @@
+"""
+Copyright (C) 2010 Ricardo Pescuma Domenecci
+
+This is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this file; see the file license.txt.  If
+not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+"""
+
+import clr
+import System
+
+clr.AddReference('System')
+
+from System.IO import File, StreamReader, StreamWriter
+
+
+SCRIPT_DIRECTORY =  __file__[:-len('_utils.py')] 
+
+
+def ToString(v):
+	if v is None:
+		return ''
+	if not isinstance(v, basestring):
+		return str(v)
+	return v
+
+def ToInt(v):
+	v = ToString(v)
+	try:
+		return int(round(float(v)))
+	except:
+		return 0
+
+def ResizeImage(image, width, height):
+	if width <= 0 and height <= 0:
+		return image
+	elif width <= 0:
+		width = image.Width * height / image.Height
+	elif height <= 0:
+		height = image.Height * width / image.Width
+	
+	result = System.Drawing.Bitmap(width, height)
+	
+	graphics = System.Drawing.Graphics.FromImage(result)
+	graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality
+	graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic
+	graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality
+	graphics.DrawImage(image, 0, 0, result.Width, result.Height)
+	
+	return result
+
+def ReadFile(path, data):
+	if not File.Exists(path):
+		return data
+	
+	file = StreamReader(path)
+	
+	try:
+		line = file.ReadLine()
+		while line:
+			line = line.strip()
+			pos = line.find('=')
+			if pos < 0:
+				continue
+			name = line[:pos].strip()
+			val = line[pos+1:].strip()
+			
+			if hasattr(data, name):
+				oldVal = getattr(data, name)
+				if isinstance(oldVal, list):
+					tmp = []
+					for v in val.split('|'):
+						tmp.append(v.strip())
+					val = tmp
+					
+				elif isinstance(oldVal, bool):
+					val = bool(val)
+			
+			setattr(data, name, val)
+			
+			line = file.ReadLine()
+		
+	finally:
+		file.Dispose()
+	
+	return data
+	
+def WriteFile(path, data):
+	file = StreamWriter(path)
+	
+	try:
+		for name in dir(data):
+			if name[0] == '_':
+				continue
+			if not hasattr(data, name):
+				continue
+			val = getattr(data, name)
+			if not val or callable(val):
+				continue
+			
+			if isinstance(val, list):
+				val = '|'.join(val)
+			
+			file.WriteLine(name + " = " + ToString(val))
+		
+	finally:
+		file.Dispose()
+
+
+_translations = { 
+	'NumIssues': 'Num of books', 
+	'ReadPercentage': 'Read' ,
+	'FullPublishers': 'Publishers/Imprints'
+	}
+
+def TranslateFieldName(name):
+	if name in _translations:
+		return _translations[name]
+	
+	ret = ''
+	for i in range(len(name)):
+		c = name[i]
+		if i > 0 and c >= 'A' and c <= 'Z':
+			ret += ' '
+		ret += c
+	return ret
+
+
+def CreateFullName(series, volume, number, count):
+	ret = CreateFullSeries(series, volume)
+	
+	_number = CreateFullNumber(number, count)
+	if _number:
+		ret += ' #' + _number
+	
+	return ret
+
+def CreateFullNumber(number, count):
+	if not number:
+		return ''
+	
+	ret = number
+	if count:
+		ret += ' of ' + count
+	
+	return ret
+
+def CreateFullSeries(series, volume):
+	if series:
+		ret = series
+	else:
+		ret = '<Unknown Series>'
+	
+	if volume:
+		if ToInt(volume) < 1900:
+			ret += ' v'+ volume
+		else:
+			ret += ' (' + volume + ')'
+	
+	return ret
+
+def CreateFullAlternateName(series, number, count):
+	_number = CreateFullNumber(number, count)
+	
+	if not series and not _number:
+		return ''
+	
+	ret = series
+	if _number:
+		ret += ' #' + _number
+	
+	return ret
+	
+def CreateFullPublisher(publisher, imprint):
+	if not publisher and not imprint:
+		return ''
+	
+	if publisher:
+		ret = publisher
+	else:
+		ret = '<Unknown Publisher>'
+	
+	if imprint:
+		ret += ' - ' + imprint
+	
+	return ret
+
+def CreateDate(month, year):
+	if not month and not year:
+		return ''
+	
+	if month:
+		ret = month + '/'
+	else:
+		ret = ''
+	
+	if year:
+		ret += year
+	else:
+		ret += '????'
+	
+	return ret
+
+
+class Placeholder:
+	pass
